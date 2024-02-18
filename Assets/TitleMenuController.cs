11b1b15adbe11fb4f7ab6ec715089fc2c1773556
Tyran Rice Jr <tyranjr@udel.edu>
2024-02-18 17:09:49 -0500

    Added title screen

---
+++ Assets/TitleMenuController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class TitleMenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject creditsMenu;
+    [SerializeField]
+    private GameObject optionsMenu;
+
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenCreditsMenu()
+    {
+        creditsMenu.SetActive(true);
+    }
+
+    public void CloseCreditsMenu()
+    {
+        creditsMenu.SetActive(false);
+    }
+
+    public void OpenOptionsMenu()
+    {
+        optionsMenu.SetActive(true);
+    }
+
+    public void CloseOptionsMenu()
+    {
+        optionsMenu.SetActive(false);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
